4c9c7182db6472df06be09dd46f52791bbc21979
DrMax <tehnolog3ooo@yandex.ru>
2022-10-13 17:59:31 +0300

    UI-2_01

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] int MaxHealth;
-    [SerializeField] int CurrentHealth;
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
 }
