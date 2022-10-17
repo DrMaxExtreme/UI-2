85869756a911f9fe3b728d5041bdd5aaa4e70cdd
DrMax <tehnolog3ooo@yandex.ru>
2022-10-17 18:08:11 +0300

    UI-2_03.1

---
+++ Assets/Scripts/DisplayHealthCount.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayHealthCount : MonoBehaviour
+{
+    [SerializeField] private HealthBar _healthBar;
+
+    public void UpdateDisplay()
+    {
+
+    }
+}
